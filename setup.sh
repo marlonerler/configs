9f1f3c1f7e7393b174a3615f16087fcd15a528cb
Marlon Erler <109803887+marlonerler@users.noreply.github.com>
2023-11-19 16:33:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,40 @@
 #!/bin/sh
 
 # base
+vim /etc/apk/repositories
+
+clear; echo "installing packages..."
 sudo apk add\
 	vim\
 	git\
 	curl\
 	tmux\
 	unzip\
-
+	nodejs\
+ 	npm\
 
 # git
+clear; echo "configuring git..."
 git config --global user.name marlonerler
 git config --global user.email viridian035@gmail.com
 
 # vim-plug
+clear; echo "installing vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+clear; echo "please set up vim"
+sleep 2
+vim .vimrc
+
+# files
+clear; echo "copying files..."
+cp .tmux.conf ~
+cp .vimrc ~
+cp .profile ~
+echo "files copied."
+
 # ssh
+clear; echo "setting up ssh key..."
 ssh-keygen
 cat ~/.ssh/id_rsa.pub
